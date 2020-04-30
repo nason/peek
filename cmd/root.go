1773413304e97c23353ed5e9afd387c0880cbc74
Landon Spear <landon@featurepeek.com>
2020-04-30 15:47:41 -0700

    upgrade go-git to v5

--- cmd/root.go
+++ cmd/root.go
@@ -34,11 +34,11 @@ import (
 	"runtime/debug"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/mholt/archiver/v3"
 	"github.com/spf13/cobra"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
