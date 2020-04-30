abf6e1f9452c4f137b369434159713a6be6d2b38
Landon Spear <landon@featurepeek.com>
2020-04-29 18:49:30 -0700

    handle arbitrary service names in peek.yml

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -59,4 +60,61 @@ func LoadFromFile(dir string) Config {
 	}
 
 	return config
+}
+
+// LoadStaticServiceFromFile attempts to populate a Service object from the peek.yml file.
+func LoadStaticServiceFromFile(dir string) (*Service, string) {
+	data, err := ioutil.ReadFile(dir + "/peek.yml")
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatal("No peek.yml config found.\n\nRun `peek init` to create one!")
+		} else {
+			log.Fatalf("Unable to read config file: %v.", err)
+		}
+	}
+
+	var config map[string]interface{}
+	if err = yaml.Unmarshal(data, &config); err != nil {
+		log.Fatalf("Unable to decode config file: %v.", err)
+	}
+
+	var staticService *Service
+	var serviceName string
+
+	for k, v := range config {
+		if k == "version" {
+			continue
+		}
+
+		s, ok := v.(map[interface{}]interface{})
+		if !ok {
+			log.Fatalln("Error parsing peek.yml")
+		}
+
+		serviceConfig := make(map[string]string)
+		for sk, sv := range s {
+			strKey := fmt.Sprintf("%v", sk)
+			strVal := fmt.Sprintf("%v", sv)
+
+			serviceConfig[strKey] = strVal
+		}
+
+		serviceType, ok := serviceConfig["type"]
+		if !ok {
+			continue
+		}
+		servicePath, ok := serviceConfig["path"]
+		if !ok {
+			continue
+		}
+		if serviceType == "static" {
+			serviceName = k
+			staticService = new(Service)
+			staticService.Type = serviceType
+			staticService.Path = servicePath
+			break
+		}
+	}
+
+	return staticService, serviceName
 }
